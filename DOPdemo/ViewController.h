13f2edd28860dfaf1cb6336dcdc4f3ac249d7dfa
tany <122074809@qq.com>
2015-03-22 20:31:41 +0800

    Initial Commit

---
+++ DOPdemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DOPdemo
+//
+//  Created by tanyang on 15/3/22.
+//  Copyright (c) 2015年 tanyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DOPdemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DOPdemo
+//
+//  Created by tanyang on 15/3/22.
+//  Copyright (c) 2015年 tanyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
