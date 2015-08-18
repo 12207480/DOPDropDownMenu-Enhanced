698b0b53673478b03229acd33461d32f2dfb6cd3
tany <122074809@qq.com>
2015-08-18 20:04:39 +0800

    add reloadData

--- DOPdemo/ViewController.m
+++ DOPdemo/ViewController.m
@@ -27,7 +27,7 @@
     // Do any additional setup after loading the view, typically from a nib.
     self.title = @"DOPDropDownMenu";
     
-    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc]initWithTitle:@"重新加载" style:UIBarButtonSystemItemDone target:self action:@selector(menuReloadData)];
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc]initWithTitle:@"重新加载" style:UIBarButtonItemStylePlain target:self action:@selector(menuReloadData)];
     // 数据
     self.classifys = @[@"美食",@"今日新单",@"电影",@"酒店"];
     self.cates = @[@"自助餐",@"快餐",@"火锅",@"日韩料理",@"西餐",@"烧烤小吃"];
